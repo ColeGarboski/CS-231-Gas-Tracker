81f70346293bdb20e7616961b35ccaa1e09ce0f2
Cole Garboski <cole.garboski@snhu.edu>
2023-03-29 09:25:13 -0400

    Adds proper SQL queries to the API

--- Gas-Tracker-App/server/index.js
+++ Gas-Tracker-App/server/index.js
@@ -1,109 +1,92 @@
-const express = require("express");
-const mysql = require("mysql");
-const cors = require("cors");
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "ColeGarboski",
-  password: "",
-  database: "gastracker",
+// Create connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'ColeGarboski',
+  password: '',
+  database: 'gastracker'
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log("Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL");
-});
+// Get all gas prices from all stations
+app.get('/gas-prices', (req, res) => {
+  const sql = `
+    SELECT 
+      s.StationName,
+      f.FuelTypeName,
+      p.Price
+    FROM 
+      Price p
+      JOIN Station s ON p.StationID = s.StationID
+      JOIN FuelType f ON p.FuelTypeID = f.FuelTypeID
+  `;
 
-// Get all items from the database
-app.get("/items", (req, res) => {
-  connection.query("SELECT * FROM price", (err, results) => {
-    if (err) {
-      console.log("Error fetching items:", err);
-      res.status(500).send("Error fetching items");
-      return;
+  pool.query(sql, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    } else {
+      res.json(results);
     }
-    res.json(results);
   });
 });
 
-// Get a specific item by ID
-app.get("/items/:id", (req, res) => {
-  const itemId = req.params.id;
-  connection.query(
-    "SELECT * FROM company WHERE CompanyID = ?",
-    [itemId],
-    (err, results) => {
-      if (err) {
-        console.log("Error fetching item:", err);
-        res.status(500).send("Error fetching item");
-        return;
-      }
-      if (results.length === 0) {
-        res.status(404).send("Item not found");
-        return;
-      }
-      res.json(results[0]);
-    }
-  );
-});
+// Get all stations for a company
+app.get('/stations/:companyName', (req, res) => {
+  const { companyName } = req.params;
 
-// Add a new item to the database
-app.post("/items", (req, res) => {
-  const newItem = req.body;
-  connection.query("INSERT INTO items SET ?", newItem, (err, result) => {
-    if (err) {
-      console.log("Error adding item:", err);
-      res.status(500).send("Error adding item");
-      return;
+  const sql = `
+    SELECT 
+      s.StationName,
+      s.StationAddress
+    FROM 
+      Station s
+      JOIN Company c ON s.CompanyID = c.CompanyID
+    WHERE 
+      c.CompanyName = ?
+  `;
+
+  pool.query(sql, [companyName], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    } else {
+      res.json(results);
     }
-    res.status(201).json({ id: result.insertId });
   });
 });
 
-// Update an existing item in the database
-app.put("/items/:id", (req, res) => {
-  const itemId = req.params.id;
-  const updatedItem = req.body;
-  connection.query(
-    "UPDATE items SET ? WHERE id = ?",
-    [updatedItem, itemId],
-    (err, result) => {
-      if (err) {
-        console.log("Error updating item:", err);
-        res.status(500).send("Error updating item");
-        return;
-      }
-      if (result.affectedRows === 0) {
-        res.status(404).send("Item not found");
-        return;
-      }
-      res.status(200).send("Item updated successfully");
-    }
-  );
-});
+// Get all prices for a station
+app.get('/prices/:stationName', (req, res) => {
+  const { stationName } = req.params;
 
-// Delete an item from the database
-app.delete("/items/:id", (req, res) => {
-  const itemId = req.params.id;
-  connection.query("DELETE FROM items WHERE id = ?", [itemId], (err, result) => {
-    if (err) {
-      console.log("Error deleting item:", err);
-      res.status(500).send("Error deleting item");
-      return;
+  const sql = `
+    SELECT 
+      f.FuelTypeName,
+      p.Price
+    FROM 
+      Price p
+      JOIN Station s ON p.StationID = s.StationID
+      JOIN FuelType f ON p.FuelTypeID = f.FuelTypeID
+    WHERE 
+      s.StationName = ?
+  `;
+
+  pool.query(sql, [stationName], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    } else {
+      res.json(results);
     }
-    if (result.affectedRows === 0) {
-      res.status(404).send("Item not found");
-      return;
-    }
-    res.status(200).send("Item deleted successfully");
   });
 });
 
