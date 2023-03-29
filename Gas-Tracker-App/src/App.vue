6e7411e6a0decf5aea2e4f23a5c93b7bd6e82aff
Cole Garboski <cole.garboski@snhu.edu>
2023-03-29 14:53:58 -0400

    Adds tailwind css

--- Gas-Tracker-App/src/App.vue
+++ Gas-Tracker-App/src/App.vue
@@ -1,85 +1,57 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <h2 class="text-6xl">Gas Prices</h2>
+    <ul>
+      <li v-for="(gasPrice, index) in gasPrices" :key="index">
+        <span>{{ gasPrice.StationName }}: </span>
+        <span>{{ gasPrice.FuelTypeName }}: </span>
+        <span>{{ getEmoji(gasPrice.FuelTypeName) }} </span>
+        <span>${{ gasPrice.Price.toFixed(2) }} </span>
+      </li>
+    </ul>
+  </div>
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const gasPrices = ref([]);
+
+async function fetchGasPrices() {
+  try {
+    const response = await axios.get('http://localhost:3000/gas-prices');
+    gasPrices.value = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Helper function to get the appropriate emoji based on the fuel type
+function getEmoji(fuelType) {
+  switch (fuelType.toLowerCase()) {
+    case 'regular':
+      return '⛽️';
+    case 'premium':
+      return '🚀';
+    case 'diesel':
+      return '🚚';
+    case 'electric':
+      return '🔌';
+    case 'compressed natural gas':
+      return '🌳';
+    case 'biofuel':
+      return '🌿';
+    default:
+      return '❓';
+  }
+}
+
+onMounted(() => {
+  fetchGasPrices();
+});
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
